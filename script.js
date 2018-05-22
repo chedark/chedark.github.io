f7ef239032d3c355b34c71529cd1d31d8bb95f68
chedark <39494934+chedark@users.noreply.github.com>
2018-05-22 23:59:21 +0200

    jjjj

---
+++ script.js
@@ -0,0 +1,57 @@
+
+
+
+var che =[{
+
+  naam:"che",
+  password: "Dit is voor mijn stage"
+}];
+var lol= prompt("Heb je honger ?");
+
+var usrn = prompt("wat is you naam ?");
+var usrp = prompt("wat is you password");
+
+function che2(naamm,pass) {
+  if (naamm === che[0].naam &&
+      pass === che[0].password){
+         return "hello che"
+  }else{
+    alert("HET INFO IS FOUT.KLIK OP OKE VOOR VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");
+alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("VIRUS");alert("goed gedaan je bent een neger");
+
+
+
+  }
+  // body...
+}
+che2(usrn,usrp);
